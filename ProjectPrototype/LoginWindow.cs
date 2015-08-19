925b5dd98dee86f59bff05646709db70831b5eb9
Ashok kumar <c.asshokkumar@gmail.com>
2015-08-19 21:54:03 +0800

    First Login Window prototype
    Just the login window prototype

---
+++ ProjectPrototype/LoginWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectPrototype
+{
+    public partial class LoginWindow : Form
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void exitOnClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
